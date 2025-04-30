b46866f643ece9ac35b99d89cc9c9f85e4d9a4f2
Germ-y <yunggi040919@naver.com>
2025-04-30 18:58:19 +0900

    Initial commit: skeleton code

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from typing import List
 
-def twoSum(nums: List[int], target: int) -> List[int]:
-    num_dict={}
-    for i,num in enumerate(nums):
-        sub=target-num
-        if sub in num_dict:
-            return [num_dict[sub], i]
-        num_dict[num]= i
+def even_list(int_list: List[int]) -> List[int]:
+    # TODO: Implement even_list
+    pass
 
-# print(twoSum([2,7,11,15],9))
-# print(twoSum([3,2,4],6))
-# print(twoSum([3,3],6))
+def sum_of_squares_of_even(even_int_list: List[int]) -> int:
+    # TODO: Implement sum_of_squares_of_even
+    pass
+
+def main():
+    int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    even_int_list = even_list(int_list)
+    output = sum_of_squares_of_even(even_int_list)
+    print(output)
+
+if __name__ == "__main__":
+    main()
